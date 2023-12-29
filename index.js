80177fa9a92f96e4db132227c6d4f8bbb91ebac0
Benjamin Finch <benjaminfinch@skiff.com>
2023-12-29 19:47:37 -0300

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+window. onload = function () {
+let text = "";
+let excuse = document.getElementById ("excuse");
+let who = ["The dog", "My grandma", "His turtle", "My bird "];
+let action = ["ate", "peed", "crushed", "broke "];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+"before the class",
+"right on time"
+"when I finished",
+"during my lunch",
+"while I was praying"
+1; You, 3 minutes ago • Uncommitted changes
+text += who [Math. trunc(Math.random () * 3)]; text += action [Math. trunc (Math. random() * 3)]; text += what [Math. trunc (Math.random() * 3)]; text += when [Math. trunc (Math. random() * 4)];
+excuse.textContent = text;
+excuse.style.fontSize = "50px";
+excuse.style.textAlign = "center"
+｝;
